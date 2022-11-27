e6a8ad49174dd3dac8c2a9cc0928672a8702e73e
max <Maximus10272@gmail.com>
2022-11-26 16:37:30 -0800

    max initial

--- Packages/profileservice.lua
+++
@@ -1,1 +0,0 @@
-return require(script.Parent._Index["nottirb_profileservice@0.1.0"]["profileservice"])
--- src/ReplicatedStorage/Settings/Settings.lua
+++
@@ -1,7 +0,0 @@
-return {
-    ["LOBBY_PLAYERS_TO_START"] = 2,
-
-    ["TEXT"] = {
-        ["GAME_STARTING"] = "game starting in %s seconds"
-    }
-}
